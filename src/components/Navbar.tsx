576b9f92123d9f779ab613c85365a01c131b8cf6
Pavan Gundlapally <gundlapallypavan2244@gmail.com>
2026-04-16 13:25:34 -0400

    Updated portfolio changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,7 +31,7 @@ const Navbar = () => {
     <>
       <div className="header">
         <a href="/#" className="navbar-title" data-cursor="disable">
-          RC
+          PKG
         </a>
 
         <a
